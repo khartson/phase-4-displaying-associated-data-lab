a4d94e59496eb2830e1d3aaf2102026aebb0fc53
Kyle <khartson2017@gmail.com>
2022-07-26 11:56:25 -0400

    commit tree eb3d5ba7948e4691ec843b9b17d09711ab20ccfb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index 
+    items = Item.all
+    render json: items, include: :user
+  end 
+
+  private 
+
+  def find_item
+    Item.find(params[:id])
+  end 
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end 
+
 end
